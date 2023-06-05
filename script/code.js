5f2cf397e84dae00d14047986aede018daeadccc
Hadou <hadoukone0102@gmail.com>
2023-06-05 19:06:47 +0000

    mis à jours

--- script/code.js
+++ script/code.js
@@ -1,8 +1,10 @@
 (function () {
 
     let menubar = document.querySelector('#menu');
+    let menuderou = document.querySelector('.navBar');
     menubar.addEventListener('click', function () {
-        console.log("merci seigneur");
+        menuderou.classList.toggle('active');
+        menubar.classList.toggle('fa-times')
     });
 
     let image = document.querySelector("#img_change");
@@ -29,5 +31,10 @@
         if (i < 4) {
             i = 1;
         }
+    })
+
+    window.addEventListener('scroll', () => {
+        menuderou.classList.remove('active');
+        menubar.classList.remove('fa-times')
     })
 })();
