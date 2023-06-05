46f69b21fe655b3124de9b7a53ee8dfe22d3598c
Hadou <hadoukone0102@gmail.com>
2023-06-05 21:51:00 +0000

    chargement ...

--- script/code.js
+++ script/code.js
@@ -12,6 +12,15 @@
     let suivant = document.querySelector("#suivant");
 
     let i = 1;
+    image.addEventListener('click', () => {
+        let result = i;
+        let chemin = `img/${result}.jpg`;
+        image.src = chemin;
+        i++;
+        if (i > 4) {
+            i = 1;
+        }
+    });
     suivant.addEventListener('click', () => {
         // Votre code 
         let result = i;
@@ -20,6 +29,33 @@
         i++;
         if (i > 4) {
             i = 1;
+        }
+        if (result == 2) {
+            let Hadou = document.querySelector('#Hadou')
+            Hadou.innerHTML = "Les Meilleurs Services Pour Vous Satisfaire";
+            let details = document.querySelector('#details')
+            details.innerHTML = `
+                HKservices est une société de développement dirigée par des professionnels passionnés, <br>
+                spécialisée dans la création de sites web, d'applications web et mobiles, ainsi que dans <br>
+                la conception d'affiches. Notre équipe expérimentée collabore étroitement avec les clients <br>
+                pour créer des solutions numériques sur mesure qui correspondent à leur identité de marque <br>
+                et offrent une expérience utilisateur optimale. Ils se concentrent sur la qualité, la créativité <br>
+                et la satisfaction du client, en offrant des services de développement innovants qui dépassent les <br>
+                attentes. Si vous recherchez des solutions numériques personnalisées pour votre entreprise, <br>
+                HKservices est là pour vous aider.
+            `;
+        } else {
+            Hadou.innerHTML = "Nous Sommes Des Développeurs Web";
+            details.innerHTML = `           
+                        Découvrez une expérience unique en vous inscrivant dès maintenant <br>
+                        sur notre site ! Rejoignez une communauté dynamique et passionnée, <br>
+                        élargissez vos horizons et profitez de nombreux avantages exclusifs.<br>
+                        Ne manquez pas cette opportunité de vous connecter avec des personnes <br>
+                        partageant les mêmes intérêts, d'accéder à un contenu premium et de participer <br>
+                        à des événements passionnants. Laissez votre empreinte dans notre communauté <br>
+                        grandissante en vous inscrivant aujourd'hui et préparez-vous à vivre une expérience <br>
+                        inoubliable
+            `;
         }
     });
 
