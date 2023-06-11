660b12224897c9995ddc3382fb78518eb932ff10
Hadou <hadoukone0102@gmail.com>
2023-06-11 19:22:29 +0000

    hadou

--- script/script_web.js
+++ script/script_web.js
@@ -14,4 +14,33 @@
             div.querySelector(moo).classList.add('active');
         })
     }
+
 })();
+
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Obtenez les valeurs des champs du formulaire
+    var form = this;
+    var name = form.name.value;
+    var email = form.email.value;
+    var message = form.message.value;
+
+    // Envoyer l'e-mail via EmailJS
+    var templateParams = {
+        from_name: name,
+        from_email: email,
+        message: message
+    };
+
+    emailjs.send('9G6jljGttPQu_tHCj', 'template_kawxqyo', templateParams)
+        .then(function (response) {
+            console.log('E-mail envoyé avec succès :', response.status);
+            // Réinitialiser le formulaire
+            form.reset();
+        }, function (error) {
+            console.error("Erreur lors de l'envoi de l'e-mail :", error);
+        });
+
+    console.log('ça passe super bien');
+});
