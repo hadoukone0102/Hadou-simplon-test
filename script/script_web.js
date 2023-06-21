e8d7351a28c3c9e6f461e3750940de433ea7658c
Hadou <hadoukone0102@gmail.com>
2023-06-21 01:09:33 +0000

    chargement ..

--- script/script_web.js
+++ script/script_web.js
@@ -47,4 +47,63 @@
         formulaire.classList.remove('active');
     })
 
+    var path = window.location.pathname;
+    const chemin_href = document.querySelectorAll('#chemin_href');
+    var url = path;
+    var filename = url.split('/').pop();
+    const tableau_chemin = [
+        'site_web.html',
+        'affiche.html',
+        'app_mobile.html'
+    ];
+
+    var fichierExiste = false;
+
+    for (let j = 0; j < tableau_chemin.length; j++) {
+
+
+        for (let k = 0; k < chemin_href.length; k++) {
+
+            if (tableau_chemin[j] === filename) {
+                fichierExiste = true;
+
+                let search_Attrib = chemin_href[k].getAttribute('href');
+                const Attribute = search_Attrib.split('/').pop();
+                if (tableau_chemin[j] === Attribute) {
+                    chemin_href[k].parentNode.style.display = 'none';
+                    break;
+                }
+            } else {
+                console.log('chemin incorrect');
+            }
+
+            break; //  Si le fichier est trouvé, on peut sortir de la boucle
+        }
+    }
+
+
+
+    if (fichierExiste) {
+        console.log("Le fichier existe dans le tableau.");
+    } else {
+        console.log("Le fichier n'existe pas dans le tableau.");
+    }
+
+    var protocol = window.location.protocol;
+    var hostname = window.location.hostname;
+    var query = window.location.search;
+
+    /*
+    console.log("Protocole :", protocol);
+    console.log("Nom d'hôte :", hostname);
+    console.log("Chemin :", path);
+    console.log("Paramètres de requête :", query);
+
+    console.log(search_Attrib)
+    console.log(Attribute)
+    */
+
+    //console.log(filename); // Résultat: "site_web.html"
+
+
 })();
